b121acbb771a1949f3c99d8603fa4daa651f42b1
topsy213 <tope_adedavids@yahoo.com>
2018-07-01 00:53:24 +0100

    Update designs.js

--- designs.js
+++ designs.js
@@ -6,12 +6,15 @@ let colorSelected;
 // Select size input
 
 $('#sizePicker').submit(function (e) {
-	
+	/**By default, submit creates and deletes the table, using preventDefault method makes the table persistent until 
+the page or window is reloaded.
+*/
 	e.preventDefault();
 	
 	rows = $('#inputHeight').val();
 	cols = $('#inputWeight').val();
-	$('#pixelCanvas').empty();
+	
+	//$('#pixelCanvas').empty();
 	
 	
 	makeGrid(rows, cols);
@@ -19,16 +22,16 @@ $('#sizePicker').submit(function (e) {
 });
 
 
-// When size is submitted by the user, call makeGrid()
+// When form is submitted by the user, call makeGrid()
 
 function makeGrid(rows, cols) {
 	
-// Your code goes here!
-	for (var i = 1; i <= rows; i++) {
+// The nested loop helps create the rows and columns and they are respectively appended to the table rows and table data.
+	for (var r = 1; r <= rows; r++) {
 		
 		$('#pixelCanvas').append('<tr></tr>');
 		
-		for (var j = 1; j <= cols; j++) {
+		for (var c = 1; c <= cols; c++) {
 			
 			$('tr:last').append('<td></td>');
 			//$('td').attr('class', 'space');
@@ -42,6 +45,7 @@ function makeGrid(rows, cols) {
 			$(this).removeAttr('style');
 		}
 		else {
+		// this adds a style attribute with background set as the current color selected.
 		$(this).attr('style', 'background-color:' +colorSelected );
 		}
 	});
@@ -50,6 +54,8 @@ function makeGrid(rows, cols) {
 
 
 /**
+I realized that Mozilla Firefox, would not reset the values in the input box when the window is reloaded,
+so I tried re-initiating the values back to 1.
 if ($('#sizePicker').reset()){
 	
 	$('#inputHeight').innerHTML == 1;
